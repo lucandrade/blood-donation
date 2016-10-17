76b9745712be68632f85174020083addc273f25c
Lucas Andrade <lucas.andrade.oliveira@hotmail.com>
2016-10-17 07:19:41 -0200

    adding unit testing

---
+++ public/tests/components/Nav/Menu.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React from 'react';
+import Menu from '../../../src/js/components/Nav/Menu';
+import { expect } from 'chai';
+import { render } from 'enzyme';
+
+describe('test', () => {
+    it('another', () => {
+        expect(true).to.equal(true);
+    });
+});
